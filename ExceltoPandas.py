025f517d180672bccffbbeab1099f980a83cd23c
RMGProjects <rcreedon@poverty-action.org>
2014-05-10 10:05:03 +0600

    Included check_unusual_lines method to workbook_checker class; allowed merge to be optional in the workbook_concatenator class. Improved doc strings

--- ExceltoPandas.py
+++ ExceltoPandas.py
@@ -55,7 +55,7 @@ class workbook_iterator(object):
 				DF_line_vals.append(str(int(line)))
 			except ValueError:
 				DF_line_vals.append(str(line))
-		DF_line_vals = [line_val if line_val != 'nan' else str(NA) for line_val in DF_line_vals]
+		DF_line_vals = [line_val if line_val != 'nan' else NA for line_val in DF_line_vals]
 		return DF_line_vals
 
 class workbook_checker(workbook_iterator):
@@ -89,8 +89,6 @@ class workbook_checker(workbook_iterator):
 		that the numerical values taken from the DataFrame in question are 
 		strings of integers (as they are in self.line_vals
 		"""
-		
-		
 		missing_lines = {DF : [] for DF in self.DFs}
 		for DF in self.DFs:
 			if numerical:
@@ -116,7 +114,7 @@ class workbook_checker(workbook_iterator):
 		DataFrame column referenced by line_col_ref.\n
 		If numerical = True then the _numerical_lines function is called to ensure
 		that the numerical values taken from the DataFrame in question are 
-		strings of integers (as they are in self.line_vals.
+		strings of integers (as they are in self.line_vals).
 		"""		
 		multiple_lines = {DF : [] for DF in self.DFs}
 		for DF in self.DFs:
@@ -129,20 +127,63 @@ class workbook_checker(workbook_iterator):
 					multiple_lines[DF].append(line)
 		multiple_lines = {key : value for key, value in multiple_lines.iteritems() if value}
 		return multiple_lines
+		
+	def check_unusual_lines(self, line_col_ref, other_lines, numerical = True)
+		"""
+		line_col_ref 	: string
+		other_lines 	: list
+		numerical 		: bool
+		return			: dict
+		method			: visible
+		
+		Function returns dict of values that has keys that are DataFrame names
+		as per self.All_DFs, and values that are lists of line names/numbers
+		that are present in the DataFrame, but are not present in either 
+		self.line_vals or the list of other_lines. The line numbers in the 
+		DataFrames are identified as being in the column referenced by line_col_ref.
+		
+		If numerical = True then the _numerical_lines function is called to ensure
+		that the numerical values taken from the DataFrame in question are 
+		strings of integers (as they are in self.line_vals).
+		"""		
+		
+		unusual_lines_dict = {DF : [] for DF in self.DFs}
+		total_lines = self.line_vals[:]
+		total_lines.extend([str(val) for val in other_lines])
+		for DF in self.DFs:
+			if numerical:
+				DF_line_vals = self._numerical_lines(self.All_DFs[DF][line_col_ref])
+				DF_line_vals = [val for val in DF_line_vals if not pd.isnull(val)] 
+			else:
+				DF_line_vals = list(self.All_DFs[DF][line_col_ref])
+				DF_line_vals = [val for val in DF_line_vals if not pd.isnull(val)]
+			unusuals = set(DF_line_vals).difference(total_lines)
+			if len(unusuals) > 0:
+				unusual_lines_dict[DF].extend([unusual for unusual in unusuals])
+		unusual_lines = {key : value for key, value in unusual_lines.iteritems() if value}
+		return unusual_lines
 	
 class workbook_concatenator(workbook_iterator):
-	def __init__(self, wkbk_file_path, line_codes, workbook_structure, line_col_ref, numerical = True):
+	def __init__(self, wkbk_file_path, line_codes, workbook_structure, line_col_ref, numerical = True, merge = True):
 		"""
 		wkbk_file_path 		: string
 		line_codes 			: DataFrame
 		workbook_structure 	: dict
 		line_col_ref		: string
+		numerical			: boolean
+		merge				: boolean
 		
-		Subclass of workbook_iterator for merging line code data with data in 
-		the DataFrames contained in the All_DFs attribute and concatenating.\n
-		This subclass adds the attribute All_DFs_merged which is a dict of DataFrames
-		as in All_DFs but merged with the line_codes information and with a 'date'
-		column added from a value accessed in the workbook_structure dict. 
+		Subclass of workbook_iterator for (optionally) merging line code data with 
+		data in rhe DataFrames contained in the All_DFs attribute and concatenating.\n
+		If you believe the values in the line_col_ref to be numerical then leave
+		numerical as True. This will replace these values with strings, so that
+		the can be merged with the line_code data which is imported as a string.\n
+		If merge is True then the data in the All_DFs will be merged with the 
+		line_code data on the line_col_ref in a 'left' merge. If it is thought that
+		this is not appropriate then set merge to False.\n		
+		This subclass adds the attribute All_DFs_merged if merge is True then this
+		will include the line_code information and a date as per the 
+		workbook_structure. If merge is False it will only add the date.  
 		"""
 		super(workbook_concatenator, self).__init__(wkbk_file_path, line_codes, workbook_structure)
 		self.All_DFs_merged = self.All_DFs.copy()
@@ -151,16 +192,28 @@ class workbook_concatenator(workbook_iterator):
 			if numerical:
 				line_Series = pd.Series(self._numerical_lines(DFM[line_col_ref]), index = DFM.index)
 				DFM[line_col_ref] = line_Series.astype(str)
-			DFM.reset_index(inplace = True)
-			DFM = DFM.merge(self.line_codes, 
-						    left_on = line_col_ref,
-							right_on = 'line',
-							how = 'left')
-			DFM.set_index(['Index1', 'Index2'], inplace = True)
+			if merge:
+				DFM.reset_index(inplace = True)
+				DFM = DFM.merge(self.line_codes, 
+								left_on = line_col_ref,
+								right_on = 'line',
+								how = 'left')
+				DFM.set_index(['Index1', 'Index2'], inplace = True)
 			DFM['date'] = self._wkbk_struc['dates'][DF]
 			self.All_DFs_merged[DF] = DFM
 			
-	def concat_all(self, drop_na_lines = True):
+	def concat_all(self, drop_na_lines = False):
+		"""
+		drop_na_lines : bool
+		return		  : DataFrame
+		
+		Method concatenates the DataFrames stored in All_DFs. If drop_na_lines is
+		False then no rows are dropped. If it is True then all rows with a null value
+		of 'line_code' will be dropped. This option is only available if the 
+		object creator has been passed merge == True. If this was not the case
+		then drop_na_lines must be False. Only drop lines if you are sure that the
+		data contain only one observation per line/day.
+		"""
 		DFC = pd.concat(self.All_DFs_merged[DF] for DF in self.DFs)
 		if drop_na_lines:
 			DFC = DFC[pd.notnull(DFC['line_code'])]
@@ -205,11 +258,27 @@ def get_line_codes(codes_file_path):
 	return line_codes
 			
 def pickle_dataframe(DF, top_folderpath, name):
+	"""
+	DF				: DataFrame
+	top_folderpath	: string
+	name			: string
+	return			: string
+	
+	Function pickles DF in top_folderpath according to name, returns sting message
+	if successful. 
+	"""
 	os.chdir(top_folderpath)
 	pickle.dump(DF, open(name, 'wb'))
 	return 'Object Pickled Sucessfully'
 	
 def unpickle_dataframe(top_folderpath, file_name):
+	"""
+	top_folderpath	: string
+	file_name		: string
+	return 			: Unpickled Object
+	
+	Function unpickles file_name from top_folderpath abd returns the result.
+	"""
 	os.chdir(top_folderpath)
 	DF = pickle.load(open(file_name, "rb"))
 	return DF
